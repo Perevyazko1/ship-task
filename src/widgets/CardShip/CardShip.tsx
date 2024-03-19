b42f50ac321e09825631e9d81cd1c1e7ca099d4c
Perevyazko <a.perevyazko@gmail.com>
2024-03-19 09:38:27 +0300

    setting hook

--- src/widgets/CardShip/CardShip.tsx
+++ src/widgets/CardShip/CardShip.tsx
@@ -6,6 +6,7 @@ import {useDataContext} from "features/DataContext";
 import cls from "./CardShip.module.scss"
 import {Vehicle} from "entity/Vehicle";
 import {useWindowWidth} from "shared/hooks/useWindowWidth/useWindowWidth";
+import {useInfiniteScroll} from "../../shared/hooks/useInfinityScroll/useInfinityScroll";
 
 interface CardShipProps {
     className?: string
@@ -14,69 +15,69 @@ interface CardShipProps {
 
 
 export const CardShip = memo((props: CardShipProps) => {
-    const widthWindow = useWindowWidth()
-    const rowShips = 330
-    const margin = 30
-    const widthCard = 440
-    const capacityWindow = Math.floor(widthWindow / (widthCard + (margin * 2)))
-    const visibleRowShips = Math.floor(window.innerHeight / (rowShips + margin) +1 )
+    // const widthWindow = useWindowWidth()
+    // const rowShips = 330
+    // const margin = 30
+    // const widthCard = 440
+    // const capacityWindow = Math.floor(widthWindow / (widthCard + (margin * 2)))
+    // const visibleRowShips = Math.floor(window.innerHeight / (rowShips + margin) +1 )
 
 
     const {loading, error, data} = useQuery(GET_VEHICLES);
     const {dataSort, setDataSort} = useDataContext()
-    const [startSlice, setStartSlice] = useState(0)
-    const [heightTransparentDiv, setHeightTransparentDiv] = useState(0)
-    const rootRef = useRef() as MutableRefObject<HTMLDivElement>
+    // const [startSlice, setStartSlice] = useState(0)
+    // const [heightTransparentDiv, setHeightTransparentDiv] = useState(0)
+    const triggerRef = useRef() as MutableRefObject<HTMLDivElement>
 
 
-    const getBottomHeight = () => {
-        if (dataSort && dataSort.length) {
-            let result = (rowShips + margin) * (Object.entries(splitArrayIntoRows(dataSort, capacityWindow)).length  - (startSlice + visibleRowShips))
-            if(result < 0){
-                return 0
-            }else {
-                return result
-            }
+    // const getBottomHeight = () => {
+    //     if (dataSort && dataSort.length) {
+    //         let result = (rowShips + margin) * (Object.entries(splitArrayIntoRows(dataSort, capacityWindow)).length  - (startSlice + visibleRowShips))
+    //         if(result < 0){
+    //             return 0
+    //         }else {
+    //             return result
+    //         }
+    //
+    //
+    //     }
+    // }
+    // const getTopHeight = () => {
+    //     return (rowShips + margin) * startSlice
+    // }
+
+    // function splitArrayIntoRows(ids: any[], rowWidth: number): { [key: string]: any[] } {
+    //     const result: { [key: string]: any[] } = {};
+    //     let currentRow = 1;
+    //     if (ids) {
+    //         for (let i = 0; i < ids.length; i += rowWidth) {
+    //             const rowKey = currentRow;
+    //             result[rowKey] = ids.slice(i, i + rowWidth);
+    //             currentRow++;
+    //         }
+    //
+    //     }
+    //     return result;
+    // }
 
 
-        }
-    }
-    const getTopHeight = () => {
-        return (rowShips + margin) * startSlice
-    }
-
-    function splitArrayIntoRows(ids: any[], rowWidth: number): { [key: string]: any[] } {
-        const result: { [key: string]: any[] } = {};
-        let currentRow = 1;
-        if (ids) {
-            for (let i = 0; i < ids.length; i += rowWidth) {
-                const rowKey = currentRow;
-                result[rowKey] = ids.slice(i, i + rowWidth);
-                currentRow++;
-            }
-
-        }
-        return result;
-    }
-
-
-    useEffect(() => {
-        function onScroll(e: any) {
-
-            setStartSlice(
-                Math.floor(e.target.scrollTop / (rowShips + margin))
-            )
-            if (Math.floor(e.target.scrollTop / (rowShips + margin)) % 2)
-                setHeightTransparentDiv(
-                    Math.floor(e.target.scrollTop / (rowShips + margin))
-                )
-        }
-        rootRef.current && rootRef.current.addEventListener('scroll', onScroll);
-
-        return () => {
-            rootRef.current && rootRef.current.removeEventListener('scroll', onScroll);
-        }
-    }, [dataSort && dataSort.length,visibleRowShips, rowShips]);
+    // useEffect(() => {
+    //     function onScroll(e: any) {
+    //
+    //         setStartSlice(
+    //             Math.floor(e.target.scrollTop / (rowShips + margin))
+    //         )
+    //         if (Math.floor(e.target.scrollTop / (rowShips + margin)) % 2)
+    //             setHeightTransparentDiv(
+    //                 Math.floor(e.target.scrollTop / (rowShips + margin))
+    //             )
+    //     }
+    //     rootRef.current && rootRef.current.addEventListener('scroll', onScroll);
+    //
+    //     return () => {
+    //         rootRef.current && rootRef.current.removeEventListener('scroll', onScroll);
+    //     }
+    // }, [dataSort && dataSort.length,visibleRowShips, rowShips]);
 
 
     const {
@@ -85,34 +86,66 @@ export const CardShip = memo((props: CardShipProps) => {
         ...otherProps
     } = props
     useEffect(() => {
+        // console.log("data",data)
         if (data?.vehicles) {
             setDataSort(data.vehicles)
         }
 
     }, [data]);
+    useEffect(() => {
 
+
+
+    }, [dataSort]);
+
+    const {
+        rowTopHeight,
+        rowBottomHeight,
+        wrapperHeight,
+        startSlice,
+        marginRow,
+        visibleRow,
+        rowHeight,
+        dataScroll
+    } = useInfiniteScroll(
+        {
+            triggerRef,
+            rowHeight: 330,
+            marginRow: 30,
+            widthElement: 440,
+            data: dataSort
+        }
+        )
+
+    //     useEffect(() => {
+    //     console.log("dataScroll",dataScroll)
+    // }, [dataScroll]);
+
+    // useEffect(() => {
+    //     console.log(data,dataScroll, dataSort)
+    // }, [data,dataScroll, dataSort]);
 
     return (
         <div
             className={classNames("", {}, [className])}
             style={{
-                height: rowShips * visibleRowShips , overflow: 'auto', justifyContent: "center",
+                height: wrapperHeight() , overflow: 'auto', justifyContent: "center",
                 display: "flex"
             }}
-            ref={rootRef}
+            ref={triggerRef}
             {...otherProps}
         >
             <div className={cls.CardShip}>
                 <div
-                    style={{height: getTopHeight(), marginTop: margin}}/>
-                {Object.entries(splitArrayIntoRows(dataSort, capacityWindow)).slice(startSlice, startSlice + visibleRowShips).map((row: any, index: number) => (
+                    style={{height: rowTopHeight(), marginTop: marginRow}}/>
+                {dataScroll && dataScroll.slice(startSlice, startSlice + visibleRow).map((row: any, index: number) => (
                         <div key={startSlice + index}
-                             style={{display: "flex", height: rowShips, marginTop: margin}}
+                             style={{display: "flex", height: rowHeight, marginTop: marginRow}}
                         >
                             {row[1].map((vehicle: Vehicle, index: number) => (
                                 <div className={cls.cardWrapper}
                                      key={index + ""}
-                                     style={{width: widthCard, marginRight: margin, marginLeft: margin}}
+                                     style={{width: 440, marginRight: marginRow, marginLeft: marginRow}}
                                 >
                                     <div className={cls.title}>{vehicle.title}</div>
                                     <img className={cls.flag} src={`https:${vehicle.nation.icons.large}`}/>
@@ -127,7 +160,7 @@ export const CardShip = memo((props: CardShipProps) => {
                         </div>
                     )
                 )}
-                <div style={{height: getBottomHeight()}}/>
+                <div style={{height: rowBottomHeight()}}/>
 
             </div>
         </div>
